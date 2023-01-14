b025c0d3209903c1642e84fe4cb3cd27b50ba24f
Khushi-013 <104719482+Khushi-013@users.noreply.github.com>
2023-01-14 11:34:08 +0530

    Output
    *
    **
    ***
    ****
    
    *
    **
    
    *
    **
    ***
    ****

---
+++ SecondClass.java
@@ -0,0 +1,19 @@
+class Pattern {
+    public static void main(String[] args) {
+        //Print the pattern using different print statements
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println();
+        //Giving Line Break
+        
+        //Print the pattern using a single print statement
+        System.out.println("*\n**");
+        System.out.println();
+        //Giving Line Break
+        
+        //Print the pattern using a single print statement Part - 2
+        System.out.println("*\n**\n***\n****");
+    }
+}
